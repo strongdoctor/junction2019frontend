b16b2d76428746f38fb95ed2db1f1eade75819d4
strongdoctor <strongdoctor@live.com>
2019-11-15 18:42:14 +0200

    Initial commit

---
+++ ClientApp/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Layout>
+    );
+  }
+}
