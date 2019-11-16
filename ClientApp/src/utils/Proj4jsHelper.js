ad0a6e422633eb8b7060d4c11dbc00933b7b74f3
strongdoctor <strongdoctor@live.com>
2019-11-16 14:39:17 +0200

    Add converter from EPSG:3067 to WGS84

---
+++ ClientApp/src/utils/Proj4jsHelper.js
@@ -0,0 +1,14 @@
+import proj4 from 'proj4/dist/proj4';
+
+class Proj4jsHelper {
+    static convertToDegrees(lat, long) {
+        const position = [lat, long];
+        position.reverse();
+        const firstProjection = '+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
+        const projectedPosition = proj4(firstProjection).inverse(position);
+        projectedPosition.reverse();
+        return projectedPosition;
+    }
+}
+
+export default Proj4jsHelper;
