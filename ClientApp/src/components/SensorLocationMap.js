0b2fc3624babb7a6dc340ae4cd6181ba36c085f1
strongdoctor <strongdoctor@live.com>
2019-11-16 14:39:35 +0200

    Make Nuuksio-map use coordinate converter utility

--- ClientApp/src/components/SensorLocationMap.js
+++ ClientApp/src/components/SensorLocationMap.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import Proj4jsHelper from '../utils/Proj4jsHelper';
 
 export default class SensorLocationMap extends Component {
   state = {
-    lng: 24.550391,
-    lat: 60.3035255,
+    lat: 6687729,
+    lng: 364641,
     zoom: 13,
   }
 
   render() {
+    //I'm not going to redefine those two in latter examples.
+    
     const position = [this.state.lat, this.state.lng]
+    const projectedPosition = Proj4jsHelper.convertToDegrees(position[0], position[1]);
+    console.log("ProjectedPosition:", projectedPosition);
     return (
       <Map
-        center={position}
+        center={projectedPosition}
         zoom={this.state.zoom}
         style={{ height: "600px" }}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={projectedPosition}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
